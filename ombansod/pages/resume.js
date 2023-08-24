7b4fcd1099dc7c73719a9da9a61bbe584e8dea3c
Nastybeing <124919100+Nastybeing@users.noreply.github.com>
2023-08-24 15:56:07 +0530

    Update resume.js

--- ombansod/pages/resume.js
+++ ombansod/pages/resume.js
@@ -6,10 +6,11 @@ import ProjectResume from "../components/ProjectResume";
 import Socials from "../components/Socials";
 import Button from "../components/Button";
 import { useTheme } from "next-themes";
-// Data
 import { name, showResume } from "../data/portfolio.json";
 import { resume } from "../data/portfolio.json";
 import data from "../data/portfolio.json";
+import resume from '../data/portfolio.json';
+
 
 const Resume = () => {
   const router = useRouter();
